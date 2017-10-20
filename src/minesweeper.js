9aa3c0b86263eab60c47ddcca6f5987fc714d323
Danny Avery <danny.w.avery@gmail.com>
2017-10-20 15:54:00 -0600

    v2.0 - Make board fully dynamic

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,17 +1,49 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join('|'));
-  console.log(board[1].join('|'));
-  console.log(board[2].join('|'));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for (var rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (var colIndex = 0; colIndex < numberOfColumns; colIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
-const board = [
-                [' ', ' ', ' '],
-                [' ', ' ', ' '],
-                [' ', ' ', ' ']
-              ];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (var rowIndex = 0; rowIndex < numberOfRows; rowIndex++) {
+    let row = [];
+    for (var colIndex = 0; colIndex < numberOfColumns; colIndex++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
 
-printBoard(board);
-board[1][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+  let numberOfBombsPlaced = 0;
+
+  while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+    board[randomRowIndex][randomColumnIndex] = 'B';
+
+    numberOfBombsPlaced++;
+
+    // An important note: The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+  }
+
+  return board;
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join('|')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
